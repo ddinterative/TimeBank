18060d42e6ef982101ce9c30d56cb9b06cb89a49
DAINGER_PC\dainger <sonic2fire@gmail.com>
2018-05-29 00:28:22 -0600

    settings completly set up

--- timeBank/app/src/main/java/com/ddinterative/timebank/settings.java
+++ timeBank/app/src/main/java/com/ddinterative/timebank/settings.java
@@ -1,9 +1,17 @@
 package com.ddinterative.timebank;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.ToggleButton;
 
 public class settings extends AppCompatActivity {
 
@@ -11,10 +19,73 @@ public class settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        getOption();
+        getTimes();
+        getToggle();
     }
 
     public void back(View v){
+
         startActivity(new Intent(settings.this, timer.class));
+    }
+
+    public void secondsToggle(View v) {
+        ToggleButton toggle = (ToggleButton) v;
+        boolean state = toggle.isChecked();
+
+        SharedPreferences mSettings = settings.this.getSharedPreferences("TimeSettings", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = mSettings.edit();
+
+        if (state) {
+            editor.putString("sec", "on");
+        }
+        else {
+            editor.putString("sec", "off");
+        }
+
+        editor.apply();
+    }
+
+    public void getToggle() {
+        SharedPreferences mSettings = settings.this.getSharedPreferences("TimeSettings", Context.MODE_PRIVATE);
+
+        String option = mSettings.getString("sec", "none");
+
+        if (option.equals("on")) {
+            ToggleButton box = (ToggleButton) findViewById(R.id.secToggle);
+            box.setChecked(true);
+        }
+    }
+
+    public void turnAddOn(View v) {
+        CheckBox add = (CheckBox)v;
+        add.setChecked(true);
+
+        CheckBox sub = (CheckBox)findViewById(R.id.subCheck);
+        sub.setChecked(false);
+
+        SharedPreferences mSettings = settings.this.getSharedPreferences("TimeSettings", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = mSettings.edit();
+
+        editor.putString("option","add");
+
+        editor.apply();
+    }
+
+    public void turnSubOn(View v) {
+        CheckBox sub = (CheckBox)v;
+        sub.setChecked(true);
+
+        CheckBox add = (CheckBox)findViewById(R.id.addCheck);
+        add.setChecked(false);
+
+        SharedPreferences mSettings = settings.this.getSharedPreferences("TimeSettings", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = mSettings.edit();
+
+        editor.putString("option","sub");
+
+        editor.apply();
     }
 
     public void openAdd(View v){
@@ -37,5 +108,120 @@ public class settings extends AppCompatActivity {
             sub.setVisibility(View.VISIBLE);
         else
             sub.setVisibility(View.GONE);
+    }
+
+    public void setAddTimes(View v) {
+        SharedPreferences mSettings = settings.this.getSharedPreferences("TimeSettings", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = mSettings.edit();
+
+        EditText addedTimeHrs = (EditText)findViewById(R.id.added_time_hrs_edit);
+        EditText addedTimeMins = (EditText)findViewById(R.id.added_time_min_edit);
+        EditText maxTimeHrs = (EditText)findViewById(R.id.max_time_hrs_edit);
+        EditText maxTimeMins = (EditText)findViewById(R.id.max_time_mins_edit);
+
+        String addedTimeHrsVal = addedTimeHrs.getText().toString();
+        String addedTimeMinsVal = addedTimeMins.getText().toString();
+        String maxTimeHrsVal = maxTimeHrs.getText().toString();
+        String maxTimeMinsVal = maxTimeMins.getText().toString();
+
+        editor.putString("addedTimeHrs",addedTimeHrsVal);
+        editor.putString("addedTimeMins",addedTimeMinsVal);
+        editor.putString("maxTimeHrs",maxTimeHrsVal);
+        editor.putString("maxTimeMins",maxTimeMinsVal);
+
+        editor.apply();
+    }
+
+    public void setSubTimes(View v) {
+        SharedPreferences mSettings = settings.this.getSharedPreferences("TimeSettings", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = mSettings.edit();
+
+        EditText initialTimeHrs = (EditText)findViewById(R.id.initial_time_hrs_edit);
+        EditText initialTimeMins = (EditText)findViewById(R.id.initial_time_mins_edit);
+        EditText decayTimeHrs = (EditText)findViewById(R.id.decay_time_hrs_edit);
+        EditText decayTimeMins = (EditText)findViewById(R.id.decay_time_mins_edit);
+        EditText goalTimeHrs = (EditText)findViewById(R.id.goal_time_hrs_edit);
+        EditText goalTimeMins = (EditText)findViewById(R.id.goal_time_mins_edit);
+
+        String initialTimeHrsVal = initialTimeHrs.getText().toString();
+        String initialTimeMinsVal = initialTimeMins.getText().toString();
+        String decayTimeHrsVal = decayTimeHrs.getText().toString();
+        String decayTimeMinsVal = decayTimeMins.getText().toString();
+        String goalTimeHrsVal = goalTimeHrs.getText().toString();
+        String goalTimeMinsVal = goalTimeMins.getText().toString();
+
+
+        editor.putString("initialTimeHrs",initialTimeHrsVal);
+        editor.putString("initialTimeMins",initialTimeMinsVal);
+        editor.putString("decayTimeHrs",decayTimeHrsVal);
+        editor.putString("decayTimeMins",decayTimeMinsVal);
+        editor.putString("goalTimeHrs",goalTimeHrsVal);
+        editor.putString("goalTimeMins",goalTimeMinsVal);
+
+        editor.apply();
+    }
+
+    public void getTimes() {
+        SharedPreferences mSettings = settings.this.getSharedPreferences("TimeSettings", Context.MODE_PRIVATE);
+
+        // get add times
+        String addedTimeHrs = mSettings.getString("addedTimeHrs", "1");
+        String addedTimeMins = mSettings.getString("addedTimeMins", "1");
+        String maxTimeHrs = mSettings.getString("maxTimeHrs", "1");
+        String maxTimeMins = mSettings.getString("maxTimeMins", "1");
+
+        // get sub times
+        String initialTimeHrs = mSettings.getString("initialTimeHrs", "1");
+        String initialTimeMins = mSettings.getString("initialTimeMins", "1");
+        String decayTimeHrs = mSettings.getString("decayTimeHrs", "1");
+        String decayTimeMins = mSettings.getString("decayTimeMins", "1");
+        String goalTimeHrs = mSettings.getString("goalTimeHrs", "1");
+        String goalTimeMins = mSettings.getString("goalTimeMins", "1");
+
+        // get add views
+        EditText addedTimeHrsView = (EditText)findViewById(R.id.added_time_hrs_edit);
+        EditText addedTimeMinsView = (EditText)findViewById(R.id.added_time_min_edit);
+        EditText maxTimeHrsView = (EditText)findViewById(R.id.max_time_hrs_edit);
+        EditText maxTimeMinsView = (EditText)findViewById(R.id.max_time_mins_edit);
+
+        // get sub views
+        EditText initialTimeHrsView = (EditText)findViewById(R.id.initial_time_hrs_edit);
+        EditText initialTimeMinsView = (EditText)findViewById(R.id.initial_time_mins_edit);
+        EditText decayTimeHrsView = (EditText)findViewById(R.id.decay_time_hrs_edit);
+        EditText decayTimeMinsView = (EditText)findViewById(R.id.decay_time_mins_edit);
+        EditText goalTimeHrsView = (EditText)findViewById(R.id.goal_time_hrs_edit);
+        EditText goalTimeMinsView = (EditText)findViewById(R.id.goal_time_mins_edit);
+
+        //set add times
+        addedTimeHrsView.setText(addedTimeHrs);
+        addedTimeMinsView.setText(addedTimeMins);
+        maxTimeHrsView.setText(maxTimeHrs);
+        maxTimeMinsView.setText(maxTimeMins);
+
+        //set sub times
+        initialTimeHrsView.setText(initialTimeHrs);
+        initialTimeMinsView.setText(initialTimeMins);
+        decayTimeHrsView.setText(decayTimeHrs);
+        decayTimeMinsView.setText(decayTimeMins);
+        goalTimeHrsView.setText(goalTimeHrs);
+        goalTimeMinsView.setText(goalTimeMins);
+
+
+    }
+
+    public void getOption() {
+        SharedPreferences mSettings = settings.this.getSharedPreferences("TimeSettings", Context.MODE_PRIVATE);
+
+        String option = mSettings.getString("option", "none");
+
+        if (option.equals("add")) {
+            CheckBox box = (CheckBox)findViewById(R.id.addCheck);
+            box.setChecked(true);
+        }
+        else if (option.equals("sub")) {
+            CheckBox box = (CheckBox)findViewById(R.id.subCheck);
+            box.setChecked(true);
+        }
+
     }
 }
